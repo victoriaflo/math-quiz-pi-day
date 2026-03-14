ca3dcd9a06dfc836263e3106640a02c45f59412b
victoriaflo <julievictoriaflorinefranklin@gmail.com>
2026-03-14 10:27:14 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,7 +2,11 @@ const questions = [
 
 {
 question:"π est un nombre infini, mais jusqu’à aujourd’hui, combien de chiffres après la virgule ont été calculés par les ordinateurs?",
-answers:["100 000 000 000 000 (100 trillions),"300 000 000 000 000 (300 trillions)","500 000 000 000 000 (500 trillions)"],
+answers:[
+"100 000 000 000 000 (100 trillions)",
+"300 000 000 000 000 (300 trillions)",
+"500 000 000 000 000 (500 trillions)"
+],
 correct:1
 },
 
@@ -13,36 +17,40 @@ correct:1
 },
 
 {
-question:"Calcule : √16 + √9 × 2 ?",
+question:"Calcule : √16 + √9 × 2",
 answers:["10","14","9"],
 correct:0
 },
 
 {
-question:"10/2 ?",
+question:"Combien font 10 ÷ 2 ?",
 answers:["2","5","8"],
 correct:1
 },
 
 {
-question:"Ton père gagne 2000 gourdes par mois, ta mère 3 fois plus et ton frère 50 % de ce que gagne ton père. Combien gagnent-ils tous ensemble par mois?",
-answers:["7500","9500","8000"],
+question:"Ton père gagne 2000 gourdes par mois, ta mère 3 fois plus et ton frère 50 % de ce que gagne ton père. Combien gagnent-ils tous ensemble ?",
+answers:["7500","9000","8000"],
 correct:1
 }
 
 ];
 
-let index=0;
+let index = 0;
 
-let score=0;
+let score = 0;
 
-const questionEl=document.getElementById("question");
+let answered = false;
 
-const answersEl=document.getElementById("answers");
+const questionEl = document.getElementById("question");
 
-const nextBtn=document.getElementById("next-btn");
+const answersEl = document.getElementById("answers");
+
+const nextBtn = document.getElementById("next-btn");
 
 function loadQuestion(){
+
+answered=false;
 
 let q=questions[index];
 
@@ -56,11 +64,26 @@ let btn=document.createElement("button");
 
 btn.textContent=answer;
 
+btn.classList.add("answer-btn");
+
 btn.onclick=function(){
+
+if(answered) return;
+
+answered=true;
 
 if(i===q.correct){
 
+btn.style.background="green";
+
 score++;
+
+}
+else{
+
+btn.style.background="red";
+
+answersEl.children[q.correct].style.background="green";
 
 }
 
@@ -85,7 +108,6 @@ if(index<questions.length){
 loadQuestion();
 
 }
-
 else{
 
 localStorage.setItem("finalScore",score);
