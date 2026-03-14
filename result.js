1c3e42e1b0240db1432d920243bbc2b040da1ede
victoriaflo <julievictoriaflorinefranklin@gmail.com>
2026-03-14 11:15:36 -0400

    Update result.js

--- result.js
+++ result.js
@@ -1,55 +1,52 @@
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function(){
 
-const score=localStorage.getItem("finalScore");
+const score = localStorage.getItem("finalScore");
 
-const scoreText=document.getElementById("final-score");
+const scoreText = document.getElementById("final-score");
 
+const whatsapp = document.getElementById("whatsapp-btn");
 
+const copy = document.getElementById("copy-btn");
 
+const url = "https://victoriaflo.github.io/math-quiz-pi-day/";
 
+// Vérification score
+if(score === null){
 
-const whatsapp=document.getElementById("whatsapp-btn");
+scoreText.textContent="Aucun résultat.";
 
-const copy=document.getElementById("copy-btn");
+return;
 
-const url="https://victoriaflo.github.io/math-quiz-pi-day/";
+}
 
-scoreText.textContent="Score : "+score+"/5 ";
+// Affichage score
+scoreText.textContent="Score : "+score+"/5 🎯";
 
-let message=
-"Math Quiz Pi Day\n\n"+
-"Score : "+score+"/5 \n\n"+
-"Essaie aussi\n"+
+// Message partage
+let message =
+"🧠 Math Quiz Pi Day\n\n"+
+"J'ai obtenu "+score+"/5 🎯\n\n"+
+"Teste toi aussi 👇\n"+
 url;
 
-retry.onclick=function(){
+// WhatsApp share
+whatsapp.addEventListener("click", function(){
 
-localStorage.removeItem("finalScore");
+let shareURL =
+"https://wa.me/?text=" +
+encodeURIComponent(message);
 
-window.location.href="quiz.html";
+window.open(shareURL,"_blank");
 
-}
+});
 
-home.onclick=function(){
-
-window.location.href="index.html";
-
-}
-
-whatsapp.onclick=function(){
-
-window.open(
-"https://wa.me/?text="+encodeURIComponent(message)
-);
-
-}
-
-copy.onclick=function(){
+// Copier lien
+copy.addEventListener("click", function(){
 
 navigator.clipboard.writeText(message);
 
-alert("Lien copié 📲");
+alert("Lien copié ! Partage maintenant 📲");
 
-}
+});
 
 });
